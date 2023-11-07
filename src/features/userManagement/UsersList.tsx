eb78b00d4061b439393472a266273e6e8b918a19
Tetiana Chernobai <yzozulya@my.com>
2023-11-07 22:31:23 +0100

    feat: add userFilter

--- src/features/userManagement/UsersList.tsx
+++ src/features/userManagement/UsersList.tsx
@@ -1,26 +1,50 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
-
-import { selectUsers } from './selectors';
-import { loadUsers } from './usersSlice';
+import { loadUsers, updateUser } from './usersSlice';
 import User from './types/User';
 import UserEditForm from './UserEditForm';
+import { selectUsers } from './selectors';
 
 export default function UsersList(): JSX.Element {
 	const users = useAppSelector<User[]>(selectUsers);
 	const dispatch = useAppDispatch();
+	const [selectedRole, setSelectedRole] = useState<string | 'All'>('All'); // Добавлено состояние для выбора роли
+
 	useEffect(() => {
 		dispatch(loadUsers());
 	}, [dispatch]);
 
+	const handleUpdate = (user: User): void => {
+		dispatch(updateUser(user));
+	};
+
+	// Метод для фильтрации пользователей по роли
+	const filteredUsers = users.filter((user) => {
+		if (selectedRole === 'All') {
+			return true;
+		} else {
+			return user.role === selectedRole;
+		}
+	});
+
 	return (
 		<div>
-			<h1>Users</h1>
+			<h1>Список Пользователей</h1>
+			{/* Выпадающий список для фильтрации */}
+			<select title="role" value={selectedRole} onChange={(e) => setSelectedRole(e.target.value)}>
+				<option value="All">All</option>
+				<option value="STUDENT">Student</option>
+				<option value="TEACHER">Teacher</option>
+				<option value="ADMIN">Admin</option>
+			</select>
 			<ul>
-				{users?.map((user: User) => (
+				{filteredUsers.map((user: User) => (
 					<li key={user.id}>
 						{user.firstName}
 						<UserEditForm user={user} />
+						<button type="button" onClick={() => handleUpdate(user)}>
+							Редактировать
+						</button>
 					</li>
 				))}
 			</ul>
