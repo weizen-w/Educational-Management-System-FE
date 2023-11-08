199a79a155f590cd1e0c5ccaab4c2c1cda563264
Wladimir Weizen <weizen.w@gmx.de>
2023-11-08 14:36:27 +0100

    G9-71 feat: add btn to lessons und style

--- src/features/groups/GroupEdit.tsx
+++ src/features/groups/GroupEdit.tsx
@@ -175,6 +175,27 @@ export default function GroupEdit(props: Props): JSX.Element {
 							</svg>
 							Archiv
 						</button>
+						<button
+							type="button"
+							className="btn btn-outline-success"
+							onClick={() => navigate('/account/groups/lessons')}
+							style={{ marginLeft: '10px' }}
+						>
+							To lessons
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="16"
+								height="16"
+								fill="currentColor"
+								className="bi bi-arrow-right-short"
+								viewBox="0 0 16 16"
+							>
+								<path
+									fillRule="evenodd"
+									d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"
+								/>
+							</svg>
+						</button>
 					</td>
 				</tr>
 			) : (
