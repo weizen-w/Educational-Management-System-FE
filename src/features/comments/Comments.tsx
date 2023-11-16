675a1052bd598476ccda728e07c96863f8169ec1
Tetiana Chernobai <yzozulya@my.com>
2023-11-17 00:40:37 +0100

    faet: add button updatedDate & Delet

--- src/features/comments/Comments.tsx
+++ src/features/comments/Comments.tsx
@@ -1,11 +1,19 @@
 import { useCallback, useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
-import Submission from '../submissions/types/Submisson';
 import { selectComments } from './selectors';
 import Comment from './types/Comment';
-import { createComment, loadCommentsBySubmission, resetCommentError } from './commentsSlice';
+import {
+	createComment,
+	loadCommentsBySubmission,
+	resetCommentError,
+	updateComment,
+	deleteComment,
+} from './commentsSlice';
 import { selectUsers } from '../users/selectors';
 import User from '../auth/types/User';
+
+import Submission from '../submissions/types/Submisson';
+import { selectUser } from '../auth/selectors';
 import { loadUsers } from '../users/usersSlice';
 
 interface Props {
@@ -34,6 +42,37 @@ export default function Comments(props: Props): JSX.Element {
 		archivedError: '',
 	});
 
+	const userId = useAppSelector(selectUser)?.id;
+
+	const handleUpdateComment = useCallback(
+		async (commentId: number) => {
+			try {
+				const updatedData = {
+					messageText: 'Новый текст комментария',
+					messageDate: '2023-11-15T12:00:00',
+					archived: true,
+				};
+				await dispatch(updateComment({ comment_id: commentId, ...updatedData } as Comment));
+				dispatch(loadCommentsBySubmission(submission.submission_id));
+			} catch (err) {
+				console.error(err);
+			}
+		},
+		[dispatch, submission.submission_id]
+	);
+
+	const handleDeleteComment = useCallback(
+		async (commentId: number) => {
+			try {
+				await dispatch(deleteComment(commentId));
+				dispatch(loadCommentsBySubmission(submission.submission_id));
+			} catch (err) {
+				console.error(err);
+			}
+		},
+		[dispatch, submission.submission_id]
+	);
+
 	const getAuthor = (findId: number): User | undefined => {
 		return users.find((u) => u.id === findId);
 	};
@@ -52,7 +91,6 @@ export default function Comments(props: Props): JSX.Element {
 				archivedError: '',
 			});
 
-			// eslint-disable-next-line @typescript-eslint/naming-convention
 			const { messageText } = newComment;
 			if (!messageText.trim()) {
 				setErrorsObj((prevErrorsObj) => ({
@@ -85,7 +123,6 @@ export default function Comments(props: Props): JSX.Element {
 				});
 				dispatch(loadCommentsBySubmission(submission.submission_id));
 			} catch (err) {
-				// eslint-disable-next-line no-console
 				console.error(err);
 			}
 		},
@@ -111,11 +148,7 @@ export default function Comments(props: Props): JSX.Element {
 				{comments
 					.toSorted((a, b) => new Date(a.messageDate).getTime() - new Date(b.messageDate).getTime())
 					.map((comment) => (
-						<a
-							key={comment.comment_id}
-							className="list-group-item list-group-item-action"
-							aria-current="true"
-						>
+						<div key={comment.comment_id}>
 							<div style={{ marginBottom: '10px', borderBottom: '1px solid black' }}>
 								<small>
 									<b>
@@ -126,34 +159,50 @@ export default function Comments(props: Props): JSX.Element {
 								<small>({new Date(comment.messageDate).toString()})</small>
 							</div>
 							<h5 className="mb-1">{comment.messageText}</h5>
-						</a>
+							<div>
+								<button
+									type="button"
+									className="btn btn-warning"
+									onClick={() => handleUpdateComment(comment.comment_id)}
+									style={{ display: comment.author_id === userId ? 'inline-block' : 'none' }}
+								>
+									Update
+								</button>
+								<button
+									type="button"
+									className="btn btn-danger"
+									onClick={() => handleDeleteComment(comment.comment_id)}
+									style={{ display: comment.author_id === userId ? 'inline-block' : 'none' }}
+								>
+									Delete
+								</button>
+							</div>
+						</div>
 					))}
 			</div>
-			<div>
-				<hr style={{ borderColor: 'black', borderWidth: '2px' }} />
-				<form className="auth-form row g-1" onSubmit={handleSubmitSend}>
-					{errorsObj.messageTextError && (
-						<div className="invalid-feedback mb-3" style={{ display: 'block' }}>
-							{errorsObj.messageTextError}
-						</div>
-					)}
-					<div className="mb-3">
-						<textarea
-							className="form-control"
-							id="exampleFormControlTextarea1"
-							rows={3}
-							value={newComment.messageText}
-							onChange={handleChangeMessage}
-							placeholder={'Comment...'}
-						></textarea>
+			<hr style={{ borderColor: 'black', borderWidth: '2px' }} />
+			<form className="auth-form row g-1" onSubmit={handleSubmitSend}>
+				{errorsObj.messageTextError && (
+					<div className="invalid-feedback mb-3" style={{ display: 'block' }}>
+						{errorsObj.messageTextError}
 					</div>
-					<div className="col-md-1 row g-1">
-						<button type="submit" className="btn btn-primary">
-							Send
-						</button>
-					</div>
-				</form>
-			</div>
+				)}
+				<div className="mb-3">
+					<textarea
+						className="form-control"
+						id="exampleFormControlTextarea1"
+						rows={3}
+						value={newComment.messageText}
+						onChange={handleChangeMessage}
+						placeholder={'Comment...'}
+					></textarea>
+				</div>
+				<div className="col-md-1 row g-1">
+					<button type="submit" className="btn btn-primary">
+						Send
+					</button>
+				</div>
+			</form>
 		</>
 	);
 }
